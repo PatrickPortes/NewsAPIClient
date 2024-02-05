51ff10e3a24267282facd113a10773a1f1f6ef04
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2024-02-05 14:42:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NewsAPIClient
+ This Android app project, named "News API Client" is built for educational purposes using Kotlin with MVVM Clean Architecture. It implements both a local data source and a remote data source. The app retrieves data from the NewsAPI (a free REST API) and displays a list of the latest news headlines from various news websites.
---
+++ app/build.gradle.kts
@@ -0,0 +1,113 @@
+import java.util.Properties
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id ("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.newsapiclient"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.newsapiclient"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        val properties = Properties()
+        properties.load(project.rootProject.file("gradle.properties").inputStream())
+        buildConfigField("String", "API_KEY", "\"${properties.getProperty("API_KEY")}\"")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // ViewModel - Kotlin
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+
+    //LiveData   - Kotlin
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+
+    //Navigation - Kotlin
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.6")
+
+    //Coroutines - Kotlin
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC2")
+
+    //Room Data Storage
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+
+    //Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+        //Lifecycle Annotation Processor
+    kapt("androidx.lifecycle:lifecycle-compiler:2.7.0")
+        //Get logs for http request and response data
+    implementation("com.squareup.okhttp3:logging-interceptor:4.5.0")
+
+    // Dagger - Hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.44")
+    kapt("androidx.hilt:hilt-compiler:1.1.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    //Test:
+        //JUnit
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+        //Espresso
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+        //Helpers for LiveData
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
+        //Helpers for Coroutines
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.2.1")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.2.1")
+        //Truth
+    testImplementation("com.google.truth:truth:1.0.1")
+    androidTestImplementation("com.google.truth:truth:1.0.1")
+        //Mockito Framework
+    testImplementation("org.mockito:mockito-core:2.25.0")
+
+}
